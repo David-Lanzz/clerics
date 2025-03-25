e0daf77bd1a95f188c40551a1298668c0c7977a4
Lanzz <lanzzd191@gmail.com>
2025-03-25 08:38:06 +0100

    update

--- src/components/about/Hero.jsx
+++ src/components/about/Hero.jsx
@@ -4,7 +4,8 @@ const Hero = () => {
     return (
         <div className='w-full flex justify-center relative px-4 md:px-[4rem] h-[50vh] md:h-[80svh] max-h-[40rem]'>
             <img src="/hand.jpg" alt="" className="absolute w-full h-full object-cover" />
-            <div className="w-full max-w-[90rem] mt-[5rem] pb-[4rem] relative flex flex-col items-start">
+            <span className="w-full h-full object-cover bg-black/50 absolute z-[2]" />
+            <div className="w-full max-w-[90rem] mt-[5rem] z-[3] pb-[4rem] relative flex flex-col items-start">
                 <h2 className="text-5xl text-secondary mt-auto font-semibold">ABOUT US</h2>
             </div>
         </div>
--- src/components/about/Perks.jsx
+++ src/components/about/Perks.jsx
@@ -6,27 +6,27 @@ const Perks = () => {
         {
             title: `Expert Team`,
             text: `Our team of experienced teachers has extensive knowledge and training in Islamic studies. We are committed to providing authentic and reliable guidance, ensuring you receive the best possible learning experience.`,
-            image: `/about/one.png`
+            image: `/about/one.jpg`
         },
         {
             title: `Personalized Approach`,
             text: `We understand that every learner is unique, which is why we take a personalized approach to every lesson. We’ll work with you to create a learning plan tailored to your specific needs and goals.`,
-            image: `/about/two.png`
+            image: `/about/two.jpg`
         },
         {
             title: `Innovative Learning`,
             text: `We use the most effective teaching methods and resources to ensure a meaningful and enriching learning experience, helping you achieve your goals with confidence.`,
-            image: `/about/middle.png`
+            image: `/about/middle.jpg`
         },
         {
             title: `Guidance Every Step`,
             text: `We’re here to support you throughout your learning journey, providing continuous guidance and encouragement to help you grow in knowledge and faith.`,
-            image: `/about/three.png`
+            image: `/about/three.jpg`
         },
         {
             title: `Commitment to Results`,
             text: `We're dedicated to helping you achieve your learning goals. We'll guide you through every step of the journey and ensure you gain valuable knowledge and confidence in your faith.`,
-            image: `/about/four.png`
+            image: `/about/four.jpg`
         },
     ];
     
@@ -38,7 +38,7 @@ const Perks = () => {
                     {
                         cards.map((card, index) => (
                             <span key={index} className={`w-full p-8 flex flex-col md:flex-row ${index % 2 === 0 ? 'md:flex-row-reverse' : null} gap-4 md:gap-8 items-center`}>
-                                <img src={card.image} className='flex-grow' alt={card.title} />
+                                <img src={card.image} className='flex-grow h-[25rem]' alt={card.title} />
                                 <span className="flex flex-col gap-4 w-full md:w-1/2">
                                     <h4 className="text-lg md:text-2xl font-semibold">{card.title}</h4>
                                     <p>{card.text}</p>
